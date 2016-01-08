f4f48d03c81b7be1df49d35cd1052451a8f1bbeb
Behram ÇELEN <behramcelen@gmail.com>
2016-01-09 01:13:06 +0200

    enable EPDisplayBundle on AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new EP\DoctrineLockBundle\EPDoctrineLockBundle(),
+            new EP\DisplayBundle\EPDisplayBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
