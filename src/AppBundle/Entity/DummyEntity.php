e30dcefd387600fb61e005a7b781e55a939173eb
Behram ÇELEN <behramcelen@gmail.com>
2016-01-09 04:42:04 +0200

    create dummy entity for bundle testing

---
+++ src/AppBundle/Entity/DummyEntity.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * ObjectLock
+ */
+class DummyEntity
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var  string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
