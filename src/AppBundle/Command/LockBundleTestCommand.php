42fa8ad9675600719213695b85bc41607d6ecbc5
Behram ÇELEN <behramcelen@gmail.com>
2016-01-10 17:26:23 +0200

    add entity based lock system tests and revise command

--- src/AppBundle/Command/LockBundleTestCommand.php
+++ src/AppBundle/Command/LockBundleTestCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use AppBundle\Entity\DummyEntity;
+use EP\DoctrineLockBundle\Exception\LockedEntityException;
 use EP\DoctrineLockBundle\Exception\LockedObjectException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,6 +18,11 @@ class LockBundleTestCommand extends ContainerAwareCommand
      */
     private $em;
 
+    /**
+     * @var
+     */
+    private $doctrine;
+
     protected function configure()
     {
         $this
@@ -30,7 +36,8 @@ class LockBundleTestCommand extends ContainerAwareCommand
      */
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
-        $this->em = $this->getContainer()->get('doctrine')->getManager();
+        $this->doctrine = $this->getContainer()->get('doctrine');
+        $this->em = $this->doctrine->getManager();
     }
 
     /**
@@ -40,11 +47,64 @@ class LockBundleTestCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        //test object based lock system
+        $this->testObjectLock($output);
+
+        //reset entity manager
+        $this->resetEntityManager();
+
+        //test entity based lock system
+        $this->testEntityLock($output);
+    }
+
+    private function testEntityLock(OutputInterface $output)
+    {
+        //create new dummy entity
+        $newDummyEntity = new DummyEntity();
+        $newDummyEntity
+            ->setTitle('Dummy Entity Title 1')
+            ->setDescription('Dummy Entity Description 1')
+            ->setUpdateLocked(true)
+            ->setDeleteLocked(true)
+        ;
+        $this->em->persist($newDummyEntity);
+        $this->em->flush();
+
+        $output->writeln('Lock entity againts update and delete process');
+
+        $output->writeln('update new dummy entity');
+        $newDummyEntity
+            ->setTitle('Updated new Dummy 1 Title')
+            ->setDescription('Updated Dummy 1 Description')
+            ->setUpdateLocked(true)
+        ;
+        $this->em->persist($newDummyEntity);
+        try{
+            $this->em->flush();
+        }catch(LockedEntityException $e){
+            $output->writeln($e->getMessage());
+        }
+
+        $this->resetEntityManager();
+        $newDummyEntity = $this->em->getRepository('AppBundle:DummyEntity')->find($newDummyEntity->getId());
+
+        $output->writeln('Remove Locked Entity!');
+        try{
+            $this->em->remove($newDummyEntity);
+        }catch(LockedEntityException $e){
+            $output->writeln($e->getMessage());
+        }
+        $output->writeln('unlock delete lock');
+        $newDummyEntity->setDeleteLocked(false);
+        $this->em->remove($newDummyEntity);
+        $this->em->flush();
+    }
+    private function testObjectLock(OutputInterface $output)
+    {
         $container = $this->getContainer();
         $objectLocker = $container->get('ep.doctrine.object.locker');
 
         $dummyEntity = new DummyEntity();
-
         $output->writeln('### TEST LOCK PROCESS ###');
         //lock fully
         $output->writeln($objectLocker->lock(new DummyEntity()));
@@ -93,7 +153,7 @@ class LockBundleTestCommand extends ContainerAwareCommand
         $dummyEntity
             ->setTitle('Dummy Title')
             ->setDescription('Dummy Description')
-            ;
+        ;
         try{
             $this->em->persist(new DummyEntity());
         }catch(LockedObjectException $e){
@@ -112,7 +172,7 @@ class LockBundleTestCommand extends ContainerAwareCommand
         $dummyEntity
             ->setTitle('Updated Dummy Title')
             ->setDescription('Updated Dummy Description')
-            ;
+        ;
         $this->em->persist($dummyEntity);
         try{
             $this->em->flush();
@@ -134,5 +194,13 @@ class LockBundleTestCommand extends ContainerAwareCommand
         $this->em->remove($dummyEntity);
 
         $this->em->flush();
+    }
+
+    private function resetEntityManager()
+    {
+        $this->em = $this->em->create(
+            $this->em->getConnection(),
+            $this->em->getConfiguration()
+        );
     }
 }
