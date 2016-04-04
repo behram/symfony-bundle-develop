aed1c8d034df91058ad3543b000fe10b197f6f86
Behram ÇELEN <behramcelen@gmail.com>
2016-04-04 06:16:07 +0300

    add array collection tests for display bundle

--- src/AppBundle/Controller/DisplayController.php
+++ src/AppBundle/Controller/DisplayController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\DummyEntity;
+use AppBundle\Entity\DummyRelation;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -14,17 +15,39 @@ class DisplayController extends Controller
      */
     public function displayAction()
     {
-        $em = $this->getDoctrine()->getManager();
-        $dummyEntity = new DummyEntity();
-        $dummyEntity
-            ->setTitle('Demo test title')
-            ->setDescription('Hello demo test dummy desctiption')
-            ;
-        $em->persist($dummyEntity);
-        $em->flush();
+        $dummyEntity = $this->createSampleEntity();
 
         return $this->render('AppBundle:Display:display.html.twig', [
             'entity' => $dummyEntity
         ]);
+    }
+
+    /**
+     * @return DummyEntity
+     */
+    private function createSampleEntity()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $dummyEntity = new DummyEntity();
+        $dummyEntity
+            ->setTitle('Demo test title')
+            ->setDescription('Hello demo test dummy description')
+        ;
+
+        foreach(range(1,15) as $value){
+            $relation = new DummyRelation();
+            $relation
+                ->setName('Display test dummy entity relation name -> '. $value)
+                ->setSummary('Display test dummy entity relation description')
+                ;
+            $relation->setDummy($dummyEntity);
+            $em->persist($relation);
+            $dummyEntity->addRelation($relation);
+        }
+
+        $em->persist($dummyEntity);
+        $em->flush();
+
+        return $dummyEntity;
     }
 }
